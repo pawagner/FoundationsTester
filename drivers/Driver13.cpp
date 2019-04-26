a2a0b8644d130088195bd2b1850f28c73fa24026
pawagner <Parker_Wagner@baylor.edu>
2019-04-26 09:13:58 -0500

    Add drivers.

---
+++ drivers/Driver13.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool M13(char);
+char Buffer[10000];
+
+int main()
+{
+   fgets(Buffer,10000,stdin);
+   while (!feof(stdin))
+   {
+      Buffer[strlen(Buffer)-1] = '\0';
+      if (strcmp(Buffer,"quit") == 0)
+      {
+         exit(0);
+      }
+      bool result;
+      result = M13('\0');
+      for (char *tc = Buffer ; *tc ; tc++)
+      {
+         result = M13(*tc);
+      }
+      fputs(Buffer,stdout);
+      if (result)
+      {
+         fputs(" - Good\n",stdout);
+      }
+      else
+      {
+         fputs(" - Bad\n",stdout);
+      }
+      fgets(Buffer,10000,stdin);
+   }
+}
